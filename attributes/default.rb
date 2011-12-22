f57650af35dfc8f2eda5c98e3677e07dbb918e57
Anthony Goddard <anthony@anthonygoddard.com>
2011-12-22 16:33:49 -0500

    initial commit

---
+++ attributes/default.rb
@@ -0,0 +1,6 @@
+default['logstash']['directory'] = '/opt/logstash'
+default['logstash']['server_role'] = 'logstash_server'
+default['logstash']['broker_role'] = 'logstash_broker'
+default['logstash']['url'] = 'http://semicomplete.com/files/logstash/logstash-1.0.17-monolithic.jar'
+default['logstash']['version'] = '1.0.17'
+default['logstash']['checksum'] = 'caba048cb1ab3cc608d4569246f8b7effbb8272865c7864a662566c30517316c'
---
+++ recipes/shipper.rb
@@ -0,0 +1,68 @@
+# Author:: Anthony Goddard
+# Author:: Phil Cryer
+#
+# Cookbook Name:: logstash
+# Recipe:: shipper
+#
+#
+# Copyright 2009, Woods Hole Marine Biologcal Laboratory
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+#make sure directory exists
+
+directory node['logstash']['directory'] do
+  owner "root"
+  group "root"
+  mode "0755"
+  action :create
+  recursive true
+end
+
+remote_file "#{node['logstash']['directory']}/logstash-#{node['logstash']['version']}-monolithic.jar" do
+  source "http://semicomplete.com/files/logstash/logstash-#{node['logstash']['version']}-monolithic.jar"
+  mode "0744"
+  checksum node['logstash']['checksum']
+end
+
+#find the broker to send the logs to
+
+search(:node, "role:#{node['logstash']['broker_role']} AND chef_environment:#{node.chef_environment}") do |n|
+  broker_host_ip = n['ipaddress']
+end
+
+#create config
+
+
+directory '/etc/logstash' do
+  owner "root"
+  group "root"
+  mode "0755"
+  action :create
+  recursive true
+end
+
+template "/etc/logstash/shipper.conf" do
+  source "shipper.conf.erb"
+  variables(
+    :broker_host_ip => broker_host_ip
+    :files => node['logstash']['files']
+  )
+end
+
+
+#start it up
+
+
