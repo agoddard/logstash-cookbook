6decc893110d76f8482acf1b5eefdd3871855008
Anthony Goddard <anthony@anthonygoddard.com>
2012-01-10 12:13:22 -0500

    added rabbitmq dependency for broker

--- metadata.rb
+++ metadata.rb
@@ -4,3 +4,4 @@ license           "Apache 2.0"
 description      "Installs/Configures logstash"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
+depends           "rabbitmq"
