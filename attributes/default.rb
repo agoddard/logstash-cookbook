3123cc3d0efa01ee559a8de6771cf6569686f6df
Anthony Goddard <anthony@anthonygoddard.com>
2012-01-10 11:40:46 -0500

    added syslog defaults

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,3 +4,11 @@ default['logstash']['broker_role'] = 'logstash_broker'
 default['logstash']['url'] = 'http://semicomplete.com/files/logstash/logstash-1.0.17-monolithic.jar'
 default['logstash']['version'] = '1.0.17'
 default['logstash']['checksum'] = 'caba048cb1ab3cc608d4569246f8b7effbb8272865c7864a662566c30517316c'
+
+default['logstash']['syslog']['debug'] = false
+default['logstash']['syslog']['listen'] = '0.0.0.0'
+default['logstash']['syslog']['format'] = ""
+default['logstash']['syslog']['message_format'] = 'json'
+default['logstash']['syslog']['port'] = 514
+default['logstash']['syslog']['tags'] = ""
+default['logstash']['syslog']['type'] = 'syslog'
