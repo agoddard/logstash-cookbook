17f269cd0c19afee308f860fe6f5c3db5ead108f
Anthony Goddard <anthony@anthonygoddard.com>
2012-01-18 10:44:35 -0500

    switched readme to md

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ maintainer       "Woods Hole Marine Biological Laboratory"
 maintainer_email "agoddard@mbl.edu"
 license           "Apache 2.0"
 description      "Installs/Configures logstash"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.4"
 depends           "java"
 depends           "rabbitmq"
